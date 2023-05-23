4e18466214f25f921f28ea58710f360a9f2fc40b
Berindeiee <adrian.berindeie10@e-uvt.ro>
2023-05-23 22:40:57 +0300

    1234

--- app/src/main/java/com/example/proiect/MainActivity.java
+++ app/src/main/java/com/example/proiect/MainActivity.java
@@ -1,29 +1,157 @@
 package com.example.proiect;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LinearLayout conversionLayout;
+    private LinearLayout conversionLayout_distanta;
+    private LinearLayout conversionLayout_masa;
+
+    private LinearLayout conversionLayout_date;
+    private LinearLayout panouFavorite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        conversionLayout = findViewById(R.id.distanta_item);
-        conversionLayout.setOnClickListener(new View.OnClickListener() {
+        conversionLayout_distanta = findViewById(R.id.distanta_item);
+        conversionLayout_masa = findViewById(R.id.masa_item);
+        conversionLayout_date = findViewById(R.id.date_item);
+        panouFavorite = findViewById(R.id.panouFavorite);
+
+
+        setMenuItem(R.id.date_item, "Date", "Convertor de date", R.drawable.date);
+
+
+        conversionLayout_distanta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, pagina_lungime.class);
                 startActivity(intent);
             }
         });
+
+        conversionLayout_masa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, pagina_masa.class);
+                startActivity(intent);
+            }
+        });
+
+        conversionLayout_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, pagina_date.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Șterge toate butoanele existente
+        panouFavorite.removeAllViews();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.proiect", MODE_PRIVATE);
+
+        boolean isPaginaLungimeFavorite = sharedPreferences.getBoolean("pagina_lungime_favorite", false);
+        if (isPaginaLungimeFavorite) {
+            // Adaugă pagina_lungime la lista de favorite
+            Button button = new Button(this);
+            button.setText("Distanță");
+            button.setBackground(ContextCompat.getDrawable(this, R.drawable.round_button)); // setează fundalul rotund
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.setMargins(0, 0, 10, 0); // setează marginile (dreapta este 10dp)
+            button.setLayoutParams(params);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, pagina_lungime.class);
+                    startActivity(intent);
+                }
+            });
+            panouFavorite.addView(button);
+        }
+
+        boolean isPaginaGreutateFavorite = sharedPreferences.getBoolean("pagina_masa_favorite", false);
+        if (isPaginaGreutateFavorite) {
+            // Adaugă pagina_greutate la lista de favorite
+            Button button = new Button(this);
+            button.setText("Masă");
+            button.setBackground(ContextCompat.getDrawable(this, R.drawable.round_button)); // setează fundalul rotund
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.setMargins(0, 0, 10, 0); // setează marginile (dreapta este 10dp)
+            button.setLayoutParams(params);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, pagina_masa.class);
+                    startActivity(intent);
+                }
+            });
+            panouFavorite.addView(button);
+        }
+
+        boolean isPaginaDateFavorite = sharedPreferences.getBoolean("pagina_date_favorite", false);
+        if (isPaginaDateFavorite) {
+            // Adaugă pagina_greutate la lista de favorite
+            Button button = new Button(this);
+            button.setText("Date");
+            button.setBackground(ContextCompat.getDrawable(this, R.drawable.round_button)); // setează fundalul rotund
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.setMargins(0, 0, 10, 0); // setează marginile (dreapta este 10dp)
+            button.setLayoutParams(params);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, pagina_date.class);
+                    startActivity(intent);
+                }
+            });
+            panouFavorite.addView(button);
+        }
+
+        // Repetă pentru fiecare activitate care poate fi adăugată la favorite
+    }
+
+    public void setMenuItem(int layoutId, String itemName, String itemCategory, int imageResourceId) {
+        // Referiți componenta folosind ID-ul pe care l-ați setat în XML
+        LinearLayout menuItemLayout = findViewById(layoutId);
+
+        // Referiți TextView-urile și ImageView din interiorul componentei
+        TextView menuItemName = menuItemLayout.findViewById(R.id.menuItemName);
+        TextView menuItemCategory = menuItemLayout.findViewById(R.id.menuItemCategory);
+        ImageView menuItemImage = menuItemLayout.findViewById(R.id.menuItemImage);
+
+        // Setează textul pentru TextView-uri și imaginea pentru ImageView
+        menuItemName.setText(itemName);
+        menuItemCategory.setText(itemCategory);
+        menuItemImage.setImageResource(imageResourceId);
+    }
+
+
 
 }
--- app/src/main/java/com/example/proiect/pagina_lungime.java
+++ app/src/main/java/com/example/proiect/pagina_lungime.java
@@ -1,35 +1,99 @@
 package com.example.proiect;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class pagina_lungime extends AppCompatActivity {
     private boolean isUpdating = false;
 
+    private Map<String, Double> toMetersConversion;
+    private Map<String, Double> fromMetersConversion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_lungime);
 
+        initializeConversionMaps();
+
+        Switch favoriteSwitch = findViewById(R.id.favorite_switch);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.proiect", MODE_PRIVATE);
+        boolean isFavorite = sharedPreferences.getBoolean("pagina_lungime_favorite", false);
+        favoriteSwitch.setChecked(isFavorite);
+
+        favoriteSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            SharedPreferences sharedPreferences1 = getSharedPreferences("com.example.proiect", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences1.edit();
+            editor.putBoolean("pagina_lungime_favorite", isChecked);
+            editor.apply();
+        });
+
+
         AutoCompleteTextView unitSpinner1 = findViewById(R.id.autoComplet1);
         AutoCompleteTextView unitSpinner2 = findViewById(R.id.autoComplet2);
         EditText valueInput1 = findViewById(R.id.value_input_1);
         EditText valueInput2 = findViewById(R.id.value_input_2);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                this,
-                R.array.distance_units,
-                android.R.layout.simple_dropdown_item_1line
-        );
+        List<String> distanceUnits = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.distance_units)));
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, distanceUnits) {
+            @Override
+            public Filter getFilter() {
+                return new Filter() {
+                    @Override
+                    protected FilterResults performFiltering(CharSequence constraint) {
+                        if (constraint != null) {
+                            ArrayList<String> suggestions = new ArrayList<String>();
+                            for (String item : getResources().getStringArray(R.array.distance_units)) {
+                                if (item.toLowerCase().contains(constraint.toString().toLowerCase())) {
+                                    suggestions.add(item);
+                                }
+                            }
+
+                            FilterResults filterResults = new FilterResults();
+                            filterResults.values = suggestions;
+                            filterResults.count = suggestions.size();
+                            return filterResults;
+                        } else {
+                            return new FilterResults();
+                        }
+                    }
+
+                    @Override
+                    protected void publishResults(CharSequence constraint, FilterResults results) {
+                        ArrayList<String> filteredList = (ArrayList<String>) results.values;
+                        if (results != null && results.count > 0) {
+                            clear();
+                            for (String c : filteredList) {
+                                add(c);
+                            }
+                            notifyDataSetChanged();
+                        }
+                    }
+                };
+            }
+        };
 
         unitSpinner1.setAdapter(adapter);
         unitSpinner2.setAdapter(adapter);
+
 
         unitSpinner1.addTextChangedListener(new TextWatcher() {
             @Override
@@ -41,7 +105,7 @@ public class pagina_lungime extends AppCompatActivity {
                 if (!isUpdating) {
                     isUpdating = true;
                     convertAndSetValues(unitSpinner1, unitSpinner2, valueInput1, valueInput2, false);
-
+                    hasShownError = false;
                 }
             }
 
@@ -61,7 +125,7 @@ public class pagina_lungime extends AppCompatActivity {
                 if (!isUpdating) {
                     isUpdating = true;
                     convertAndSetValues(unitSpinner1, unitSpinner2, valueInput1, valueInput2, true);
-
+                    hasShownError = false;
                 }
             }
 
@@ -112,6 +176,57 @@ public class pagina_lungime extends AppCompatActivity {
             }
         });
 
+
+        unitSpinner1.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                unitSpinner1.setText("");
+            }
+        });
+        unitSpinner2.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                unitSpinner2.setText("");
+            }
+        });
+
+        unitSpinner1.setOnClickListener(v -> unitSpinner1.setText(""));
+        unitSpinner2.setOnClickListener(v -> unitSpinner2.setText(""));
+
+
+    }
+    private void initializeConversionMaps() {
+        toMetersConversion = new HashMap<>();
+        toMetersConversion.put("Milimetru (mm)", 1/1000.0);
+        toMetersConversion.put("Centimetru (cm)", 1/100.0);
+        toMetersConversion.put("Decimetru (dm)", 1/10.0);
+        toMetersConversion.put("Metru (m)", 1.0);
+        toMetersConversion.put("Decametru (dam)", 10.0);
+        toMetersConversion.put("Hectometru (hm)", 100.0);
+        toMetersConversion.put("Kilometru (km)", 1000.0);
+        toMetersConversion.put("Inch (in)", 0.0254);
+        toMetersConversion.put("Picior (ft)", 0.3048);
+        toMetersConversion.put("Yard (yd)", 0.9144);
+        toMetersConversion.put("Milă (mi)", 1609.344);
+        toMetersConversion.put("Milă marină (nmi)", 1852.0);
+        toMetersConversion.put("Unitate astronomică (AU)", 149597870700.0);
+        toMetersConversion.put("An-lumină (ly)", 9.461e+15);
+        toMetersConversion.put("Parsec (pc)", 3.086e+16);
+
+        fromMetersConversion = new HashMap<>();
+        fromMetersConversion.put("Milimetru (mm)", 1000.0);
+        fromMetersConversion.put("Centimetru (cm)", 100.0);
+        fromMetersConversion.put("Decimetru (dm)", 10.0);
+        fromMetersConversion.put("Metru (m)", 1.0);
+        fromMetersConversion.put("Decametru (dam)", 1/10.0);
+        fromMetersConversion.put("Hectometru (hm)", 1/100.0);
+        fromMetersConversion.put("Kilometru (km)", 1/1000.0);
+        fromMetersConversion.put("Inch (in)", 1/0.0254);
+        fromMetersConversion.put("Picior (ft)", 1/0.3048);
+        fromMetersConversion.put("Yard (yd)", 1/0.9144);
+        fromMetersConversion.put("Milă (mi)", 1/1609.344);
+        fromMetersConversion.put("Milă marină (nmi)", 1/1852.0);
+        fromMetersConversion.put("Unitate astronomică (AU)", 1/149597870700.0);
+        fromMetersConversion.put("An-lumină (ly)", 1/9.461e+15);
+        fromMetersConversion.put("Parsec (pc)", 1/3.086e+16);
     }
 
     private void convertAndSetValues(AutoCompleteTextView fromUnitSpinner, AutoCompleteTextView toUnitSpinner, EditText fromValueInput, EditText toValueInput, boolean reverse) {
@@ -121,7 +236,7 @@ public class pagina_lungime extends AppCompatActivity {
         EditText output = reverse ? fromValueInput : toValueInput;
         String inputValue = input.getText().toString();
 
-        if (!inputValue.isEmpty()) {
+        if (!inputValue.isEmpty() && isNumeric(inputValue)) {
             double value = Double.parseDouble(inputValue);
             double result = convertDistance(value, fromUnit, toUnit);
             output.setText(String.valueOf(result));
@@ -131,84 +246,53 @@ public class pagina_lungime extends AppCompatActivity {
         }
     }
 
+    private boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
 
     private double convertDistance(double value, String fromUnit, String toUnit) {
         double meters = toMeters(value, fromUnit);
-        double result = fromMeters(meters, toUnit);
-        return result;
+        return fromMeters(meters, toUnit);
     }
 
+    private long lastToastTime = 0;
+
+
+    private boolean hasShownError = false;
+
     private double toMeters(double value, String unit) {
-        switch (unit) {
-            case "Milimetru (mm)":
-                return value / 1000;
-            case "Centimetru (cm)":
-                return value / 100;
-            case "Decimetru (dm)":
-                return value / 10;
-            case "Metru (m)":
-                return value;
-            case "Decametru (dam)":
-                return value * 10;
-            case "Hectometru (hm)":
-                return value * 100;
-            case "Kilometru (km)":
-                return value * 1000;
-            case "Inch (in)":
-                return value * 0.0254;
-            case "Picior (ft)":
-                return value * 0.3048;
-            case "Yard (yd)":
-                return value * 0.9144;
-            case "Milă (mi)":
-                return value * 1609.344;
-            case "Milă marină (nmi)":
-                return value * 1852;
-            case "Unitate astronomică (AU)":
-                return value * 149597870700L;
-            case "An-lumină (ly)":
-                return value * 9.461e+15;
-            case "Parsec (pc)":
-                return value * 3.086e+16;
-            default:
-                return 0;
+        if (!toMetersConversion.containsKey(unit)) {
+            if (!hasShownError) {
+                long currentTime = System.currentTimeMillis();
+                if (currentTime - lastToastTime > 4000) { // Verificăm dacă au trecut cel puțin 2 secunde de la ultimul toast
+                    Toast.makeText(this, "Unitatea de măsură nu este recunoscută.", Toast.LENGTH_SHORT).show();
+                    lastToastTime = currentTime; // Actualizăm timpul ultimului toast
+                }
+                hasShownError = true;
+            }
+            return 0;
         }
+        return value * toMetersConversion.get(unit);
     }
 
     private double fromMeters(double meters, String unit) {
-        switch (unit) {
-            case "Milimetru (mm)":
-                return meters * 1000;
-            case "Centimetru (cm)":
-                return meters * 100;
-            case "Decimetru (dm)":
-                return meters * 10;
-            case "Metru (m)":
-                return meters;
-            case "Decametru (dam)":
-                return meters / 10;
-            case "Hectometru (hm)":
-                return meters / 100;
-            case "Kilometru (km)":
-                return meters / 1000;
-            case "Inch (in)":
-                return meters / 0.0254;
-            case "Picior (ft)":
-                return meters / 0.3048;
-            case "Yard (yd)":
-                return meters / 0.9144;
-            case "Milă (mi)":
-                return meters / 1609.344;
-            case "Milă marină (nmi)":
-                return meters / 1852;
-            case "Unitate astronomică (AU)":
-                return meters / 149597870700L;
-            case "An-lumină (ly)":
-                return meters / 9.461e+15;
-            case "Parsec (pc)":
-                return meters / 3.086e+16;
-            default:
-                return 0;
+        if (!fromMetersConversion.containsKey(unit)) {
+            if (!hasShownError) {
+                long currentTime = System.currentTimeMillis();
+                if (currentTime - lastToastTime > 4000) { // Verificăm dacă au trecut cel puțin 2 secunde de la ultimul toast
+                    Toast.makeText(this, "Unitatea de măsură nu este recunoscută.", Toast.LENGTH_SHORT).show();
+                    lastToastTime = currentTime; // Actualizăm timpul ultimului toast
+                }
+                hasShownError = true;
+            }
+            return 0;
         }
+        return meters * fromMetersConversion.get(unit);
     }
+
 }
